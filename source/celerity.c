bca08840d21bd2828230d3e3927b3ae26aee4fda
f <f>
2000-08-15 16:31:40 +0000

    Code cleanup.

--- source/celerity.c
+++ source/celerity.c
@@ -4,7 +4,7 @@
  *  code:      jeremy roethel [xanth@3sheep.COM]
  *  cosmetics: sasha eysymontt [sage@3Sheep.COM]
  *
- *  $Id: celerity.c,v 1.3 2000-08-15 16:03:07 f Exp $
+ *  $Id: celerity.c,v 1.4 2000-08-15 16:31:40 f Exp $
  */
 /*
  * Cstatusbar()		- /set STATUSBAR #
@@ -15,6 +15,7 @@
 #ifdef CELE
 #include "irc.h"
 #include "ircaux.h"
+#include "output.h"
 #include "screen.h"
 #include "status.h"
 #include "myvars.h" 
@@ -26,47 +27,52 @@
 /* handled in include files above */
 
 /* Global Stuff */
-void Cstatusbar _((int));
+void Cstatusbar _((char *,char *,char *));
 void Cquickstat _((char *,char *,char *));
 extern void Cquick_status _((char *, int));
 
 char *CelerityVersion=" celerity v1.1";
 char *CelerityL="\002/cy/\002";
 
-int Cqstat=0; /* Status of QuickStat: 1=on, 0=off */
+static int Cqstat=0; /* Status of QuickStat: 1=on, 0=off */
 
 /* Internal Stuff */
-static int Cstatusnum=2;
+#define Cstatusnum 2
 
 
 /******************** Beginning of code section ********************/
 
-/* Change the status bar [/set STATUSBAR] 
-void Cstatusbar(n)
-int n;
+/* Change the status bar [/set STATUSBAR] */
+void Cstatusbar(command,args,subargs)
+char *command;
+char *args;
+char *subargs;
 {
-    unsigned int display;
-    char tmpbuf[mybufsize/4];
-    char newbar0[mybufsize];
-    char newbar1[mybufsize];
-    char newbar2[mybufsize];
-    char newbar3[mybufsize];
+    int n=0;
+    u_int old_display;
+    char *sbar=(char *) 0;
+    char *newbar0;
+    char *newbar1;
+    char *newbar2;
+    char *newbar3;
+    char tmpbuf[mybufsize/2];
 
-    if ((n > 0) && (n <= Cstatusnum)) {
+    sbar=new_next_arg(args,&args);
+    if (sbar) n=atoi(sbar);
+    if ((n>0) && (n<=Cstatusnum)) {
 	if (n==2) {
-	    sprintf(newbar0,"%y1 [OperVision] %> %%!S ");
-	    sprintf(newbar1,"%y1 %%T %%*%@%y3%%N %%# %%C%%+%%A %%Q %%> %%M ");
-	    sprintf(newbar2,"%y1 %%1 %%> %%S%%H%%B%%I%%O%%F%%W l/%%2 ");
-	    sprintf(newbar3,"%y1 %%U [lj:%%3] %%> [DCC:s%%6:r%%5:o%%4] ");
+	    newbar0="%y1 [OperVision] %> %!S ";
+	    newbar1="%y1 %T %*%@%y3%N %# %C%+%A %Q %> %M ";
+	    newbar2="%y1 %1 %> %S%H%B%I%O%F%W l/%2 ";
+	    newbar3="%y1 %U [lj:%3] %> [DCC:s%6:r%5:o%4] ";
 	}
-	else { 		*//* bar 1 is the default 
-	    sprintf(newbar0,"%y1 [OperVision] %%> %%!S ");
-	    sprintf(newbar1,"%y1 %%T %%*%%@%y3%%N %%#%%A %%Q %%> %%M %%1 ");
-	    sprintf(newbar2,"%y1 %%C%%+ %%U %%> %%S%%H%%B%%I%%O%%F %%W l/%%2 ");
-	    sprintf(newbar3,"%y1 QuickStat¯ %%> [lj:%%3] [DCC:s%%6:r%%5:o%%4] ");
+	else { 		/* bar 1 is the default */
+	    newbar0="%y1 [OperVision] %> %!S ";
+	    newbar1="%y1 %T %*%@%y3%N %#%A %Q %> %M %1 ";
+	    newbar2="%y1 %C%+ %U %> %S%H%B%I%O%F %W l/%2 ";
+	    newbar3="%y1 QuickStat¯ %> [lj:%3] [DCC:s%6:r%5:o%4] ";
 	}
-
-	display=window_display;
+	old_display=window_display;
 	window_display=0;
 	sprintf(tmpbuf,"STATUS_FORMAT %s",newbar0);
 	set_variable(NULL,tmpbuf,NULL);
@@ -76,39 +82,28 @@ int n;
 	set_variable(NULL,tmpbuf,NULL);
 	sprintf(tmpbuf,"STATUS_FORMAT3 %s",newbar3);
 	set_variable(NULL,tmpbuf,NULL);
-	window_display=display;
-
-	set_int_var(STATUSBAR_VAR,n);
-    } else if (n < 0 || n > Cstatusnum) 
-	say("Error: Invalid status bar number (1 - %d)",Cstatusnum);
+	window_display=old_display;
+    }
+    else say("Error: Invalid status bar number (1 - %d)",Cstatusnum);
 }
-*/
+
 void Cquickstat(command,args,subargs)
 char *command;
 char *args;
 char *subargs;
 {
-    Window *window;
-    int current;
+    char tmpbuf[mybufsize/16];
 
-    window = curr_scr_win;
-    current = window->double_status;
-
-    if (Cqstat==1) {	/* Turn off */
+    if (Cqstat) {	/* Turn off */
 	Cqstat=0;
-	window->double_status=1;
-	window->display_size+=current-window->double_status;
-	recalculate_window_positions();
-	update_all_windows();
-	build_status((char *) 0);
+        strcpy(tmpbuf,"DOUBLE 2");
     }
     else {		/* Turn on */
 	Cqstat=1;
-	Cquick_status((char *) 0,Cqstat);
-	window->double_status=2;
-	window->display_size+=current-window->double_status;
-	recalculate_window_positions();
-	update_all_windows();
+        strcpy(tmpbuf,"DOUBLE 3");
     }
+    Cquick_status((char *) 0,Cqstat);
+    windowcmd(NULL,tmpbuf,NULL);
+    update_all_status();
 }
 #endif
