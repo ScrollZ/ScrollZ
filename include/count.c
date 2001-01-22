8b969ccd6451fb6150ce3f34da5c057881e53521
f <f>
2001-01-22 17:51:05 +0000

    Fixed code to avoid warnings when compiling.

--- include/count.c
+++ include/count.c
@@ -5,13 +5,13 @@
 #include <stdio.h>
 #include <string.h>
 
-void main() {
+int main() {
     int  count=0;
     char buffer[128]; /* should be more than enough */
     char *tmpstr;
     FILE *fp;
 
-    while ((fgets(buffer,128,stdin)))
+    while ((fgets(buffer,128,stdin))) {
         /* valid lines start with #define */
         if (!strncmp(buffer,"#define",7) && (tmpstr=strstr(buffer,"$\n")) &&
             *(tmpstr+2)=='\0') {
@@ -20,4 +20,6 @@ void main() {
             count++;
         }
 	else fprintf(stdout,"%s",buffer);
+    }
+    return(0);
 }
