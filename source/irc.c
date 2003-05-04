ece9c60f470f3617427536674b0e32ac1c50b8bf
f <f>
2003-05-04 19:37:49 +0000

    Check in for 1.9.1beta1.

--- source/irc.c
+++ source/irc.c
@@ -31,12 +31,12 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $Id: irc.c,v 1.107 2003-05-04 18:06:50 f Exp $
+ * $Id: irc.c,v 1.108 2003-05-04 19:37:49 f Exp $
  */
 
-#define IRCII_VERSION	"20020310"	/* YYYYMMDD */
+#define IRCII_VERSION	"20030505"	/* YYYYMMDD */
 /**************************** PATCHED by Flier ******************************/
-#define SCROLLZ_VERSION "1.9"
+#define SCROLLZ_VERSION "1.9.1beta1"
 /****************************************************************************/
 
 #include "irc.h"
@@ -265,7 +265,7 @@ char  defban;
 char  bold=2;
 char  *DefaultServer=(char *) 0;
 char  *ScrollZstr=(char *) 0;
-char  *ScrollZver="ircII " IRCII_VERSION "+ScrollZ " SCROLLZ_VERSION " (22.12.2002)+Cdcc v" CDCC_VERSION;
+char  *ScrollZver="ircII " IRCII_VERSION "+ScrollZ " SCROLLZ_VERSION " (5.4.2003)+Cdcc v" CDCC_VERSION;
 char  *ScrollZver1=(char *) 0;
 #ifdef EXTRA_STUFF
 char  *EString=(char *) 0;
