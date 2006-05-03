d740b21eaa380c4642b6f9af9ef3ad900e1d3222
f <f>
2006-05-03 16:49:47 +0000

    Check-in for new version. Bump version number.

--- source/irc.c
+++ source/irc.c
@@ -31,12 +31,12 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $Id: irc.c,v 1.125 2006-04-30 14:20:56 f Exp $
+ * $Id: irc.c,v 1.126 2006-05-03 16:49:47 f Exp $
  */
 
-#define IRCII_VERSION	"20060501"	/* YYYYMMDD */
+#define IRCII_VERSION	"20060504"	/* YYYYMMDD */
 /**************************** PATCHED by Flier ******************************/
-#define SCROLLZ_VERSION "1.9.98"
+#define SCROLLZ_VERSION "1.9.98.1"
 /****************************************************************************/
 
 #include "irc.h"
@@ -265,7 +265,7 @@ char  defban;
 char  bold=2;
 char  *DefaultServer=(char *) 0;
 char  *ScrollZstr=(char *) 0;
-char  *ScrollZver="ircII " IRCII_VERSION "+ScrollZ " SCROLLZ_VERSION " (01.05.2006)+Cdcc v" CDCC_VERSION;
+char  *ScrollZver="ircII " IRCII_VERSION "+ScrollZ " SCROLLZ_VERSION " (04.05.2006)+Cdcc v" CDCC_VERSION;
 char  *ScrollZver1=(char *) 0;
 #ifdef EXTRA_STUFF
 char  *EString=(char *) 0;
