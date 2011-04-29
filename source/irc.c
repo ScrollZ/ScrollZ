6446388228eac37a3badfc46ef7b3ab6622bd0e1
f <f@confused.org>
2011-04-29 16:20:03 +0200

    Bump version number and release date string.

--- source/irc.c
+++ source/irc.c
@@ -34,9 +34,9 @@
  * $Id: irc.c,v 1.138 2009-12-21 15:08:26 f Exp $
  */
 
-#define IRCII_VERSION	"20091223"	/* YYYYMMDD */
+#define IRCII_VERSION	"20110514"	/* YYYYMMDD */
 /**************************** PATCHED by Flier ******************************/
-#define SCROLLZ_VERSION "2.1"
+#define SCROLLZ_VERSION "2.2"
 /****************************************************************************/
 
 #include "irc.h"
@@ -265,7 +265,7 @@ char  defban;
 char  bold=2;
 char  *DefaultServer=(char *) 0;
 char  *ScrollZstr=(char *) 0;
-char  *ScrollZver="ircII " IRCII_VERSION "+ScrollZ " SCROLLZ_VERSION " (23.12.2009)+Cdcc v" CDCC_VERSION;
+char  *ScrollZver="ircII " IRCII_VERSION "+ScrollZ " SCROLLZ_VERSION " (14.5.2011)+Cdcc v" CDCC_VERSION;
 char  *ScrollZver1=(char *) 0;
 #ifdef EXTRA_STUFF
 char  *EString=(char *) 0;
