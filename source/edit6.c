ScrollZ/ScrollZ

commit 0e5977a36aed1e0a85b2c6c2d95dfc9ebdf378cd

Fix crash when input buffer exceeded 1024 characters and AUTOCOMPL is enabled.

source/edit6.c
@@ -333,7 +333,7 @@ ChannelList *tmpchan;
     char *tmp=AutoReplyString;
     char *mynick;
     char *tmpstr=result;
-    char tmpbuf[mybufsize];
+    char *tmpbuf = (char *) new_malloc(strlen(line) + 256);
     NickList *nick=NULL;
     NickList *tmpnick;
 
@@ -367,10 +367,12 @@ ChannelList *tmpchan;
             if (tmp && *tmp) strmcat(tmpbuf,tmp,sizeof(tmpbuf));
             strmcat(tmpbuf,tmpstr,sizeof(tmpbuf));
             strcpy(result,tmpbuf);
+            new_free(&tmpbuf);
             return;
         }
     }
     strcpy(result,line);
+    new_free(&tmpbuf);
 }
 
 /* Removes nick from DCC list */

source/edit.c
@@ -3326,7 +3326,7 @@ do_send_text(command, args, subargs)
 {
 	char	*tmp;
 /**************************** PATCHED by Flier ******************************/ 
-        char    tmpbuf[mybufsize];
+        char    *tmpbuf = NULL;
         ChannelList *tmpchan;
 /****************************************************************************/
 
@@ -3344,11 +3344,15 @@ do_send_text(command, args, subargs)
 /**************************** PATCHED by Flier ******************************/ 
         if (AutoNickCompl && tmp && args && *args &&
             (tmpchan=lookup_channel(tmp,curr_scr_win->server,0))) {
+            tmpbuf = (char *) new_malloc(strlen(args) + 256);
             AutoNickComplete(args,tmpbuf,tmpchan);
             args=tmpbuf;
         }
 /****************************************************************************/
 	send_text(tmp, args, NULL);
+/**************************** PATCHED by Flier ******************************/ 
+    new_free(&tmpbuf);
+/****************************************************************************/
 }
 
 /*

commit 7c065dfc771472fb12fc1ef996b691e9e644a70e

Fix auto completion bug introduced in commit 0e5977a36aed1e0a85b2c6c2d95dfc9ebdf378cd.

source/edit6.c
@@ -330,10 +330,11 @@ ChannelList *tmpchan;
 {
     int  len;
     int  count=0;
+    int  linelen = strlen(line) + 256;
     char *tmp=AutoReplyString;
     char *mynick;
     char *tmpstr=result;
-    char *tmpbuf = (char *) new_malloc(strlen(line) + 256);
+    char *tmpbuf = (char *) new_malloc(linelen);
     NickList *nick=NULL;
     NickList *tmpnick;
 
@@ -361,11 +362,11 @@ ChannelList *tmpchan;
            was only one match found */
         if (count==1 || (AutoNickCompl==2 && count)) tmpnick=NULL;
         if (!tmpnick && nick) {
-            if (count==1 || AutoNickCompl==2) strmcpy(tmpbuf,nick->nick,sizeof(tmpbuf));
-            else if (AutoNickCompl==1) strmcpy(tmpbuf,result,sizeof(tmpbuf));
+            if (count==1 || AutoNickCompl==2) strmcpy(tmpbuf,nick->nick,linelen);
+            else if (AutoNickCompl==1) strmcpy(tmpbuf,result,linelen);
             if (*tmpstr==' ' && tmp && *(tmp+1)==' ') tmpstr++;
-            if (tmp && *tmp) strmcat(tmpbuf,tmp,sizeof(tmpbuf));
-            strmcat(tmpbuf,tmpstr,sizeof(tmpbuf));
+            if (tmp && *tmp) strmcat(tmpbuf,tmp,linelen);
+            strmcat(tmpbuf,tmpstr,linelen);
             strcpy(result,tmpbuf);
             new_free(&tmpbuf);
             return;