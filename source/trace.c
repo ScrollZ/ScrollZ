b61d31c1f034a9c8ed30cad073a191e063e95d63
flier <justflier@gmail.com>
2013-05-02 09:52:16 +0200

    Fix crash when server name is not available. Added help for SET TRACE.

--- source/trace.c
+++ source/trace.c
@@ -159,7 +159,8 @@ char *arg10;
     if (!fp)
         return;
 
-    srvname = get_server_name(parsing_server_index);
+    if (parsing_server_index != -1) srvname = get_server_name(parsing_server_index);
+    else srvname = "N/A";
     strftime(timestr, sizeof(timestr), "%Y-%m-%d %H:%M:%S", localtime(&timenow));
     fprintf(fp, "%s [%s] %s: ", timestr, areaname, srvname);
 
